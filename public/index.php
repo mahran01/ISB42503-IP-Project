667c1c27a71212cd436d8123122dc48334a74a55
Mahran Mazlan <m6mmbm.uni@gmail.com>
2023-06-04 23:09:23 +0800

    Init project

---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php 
+$page_title="Index";
+$root_url = "../";
+?>
+<?php include($root_url.'/src/includes/header.html');?>
+<h1>Index</h1>
+<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis et necessitatibus voluptatibus dolore officia nisi illum unde officiis vero. Nemo hic, perferendis sapiente facere corporis saepe libero vitae veniam ea!</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos illum, consequatur quas sint nulla nihil dolore aliquam et expedita delectus, in, unde vero omnis itaque cumque. Provident repudiandae perferendis voluptate.</p>
+<?php include('../src/includes/footer.html');?>
