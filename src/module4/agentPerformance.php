c901dbe44e67bbe145810594a34685dc8c985ddf
Nadziman <z6578010@gmail.com>
2023-06-08 17:25:52 +0800

    update agent performance

--- src/module4/agentPerformance.php
+++ src/module4/agentPerformance.php
@@ -89,6 +89,51 @@
 				<td align="center">' . $result_sales['total_sales'] . '</td>
 				</tr>
 			</table>';
+
+			$q = "SELECT * FROM sales_order INNER JOIN sales_order_line USING (SalesOrderId) INNER JOIN approval USING (ApprovalId) INNER JOIN item USING (ItemId) WHERE approval.ApprovalStatusId = 2 AND sales_order.CreatedBy = $agent";
+			$r = mysqli_query($dbc, $q) or die ('error');
+			$num = mysqli_num_rows($r);
+				if(mysqli_num_rows($r) > 0){
+
+			echo '<br/><b>Customers list:</b><br/>';
+			echo '<table border="3" align="center" cellspacing="2" cellpadding="5" class="custom-table">
+						<tr>
+							<td align="left"><b>Sales Order ID</b></td>
+							<td align="left"><b>Customer Name</b></td>
+							<td align="left"><b>Contact Number</b></td>
+							<td align="left"><b>Date Create</b></td>
+							<td align="left"><b>Created By</b></td>
+							<td align="left"><b>Item ID</b></td>
+							<td align="left"><b>Item Name</b></td>
+							<td align="left"><b>Quantity</b></td>
+						</tr>';
+			
+			$prev = "";
+			while ($row = mysqli_fetch_assoc($r)){
+		
+				$isSame = $prev === $row['SalesOrderId'];
+				
+				$salesOrderId = $isSame ? "" : $row['SalesOrderId'];
+				$customerName = $isSame ? "" : $row['CustomerName'];
+				$contactNumber = $isSame ? "" : $row['ContactNumber'];
+				$dateCreated = $isSame ? "" : $row['DateCreated'];
+				$createdBy = $isSame ? "" : $row['CreatedBy'];
+		
+				echo '<tr>
+					<td align="left">' .$salesOrderId. '</td>
+					<td align="left">' .$customerName.'</td>
+					<td align="left">' .$contactNumber. '</td>
+					<td align="left">' .$dateCreated. '</td>
+					<td align="left">' .$createdBy. '</td>
+					<td align="left">' . $row['ItemId'] . '</td>
+					<td align="left">' . $row['ItemName'] . '</td>
+					<td align="left">' . $row['Quantity'] . '</td>
+				</tr>';
+		
+				$prev = $salesOrderId;
+			}
+			} 
+			echo '</table>';
 	}
 
 ?>
